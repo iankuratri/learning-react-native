bd15fb39e38b3bd84802f3e00b47c9ee6f65ada3
Ankur Atri <ankuratri.in@gmail.com>
2023-07-18 12:37:26 +0530

    Touchable component added

--- DoneWithIt/App.tsx
+++ DoneWithIt/App.tsx
@@ -1,27 +1,36 @@
 import { StatusBar } from "expo-status-bar";
-import { Image, SafeAreaView, StyleSheet, Text } from "react-native";
+import {
+  Image,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TouchableHighlight,
+} from "react-native";
 
 export default function App() {
-  const handlePress = () => {
-    console.log("Text pressed");
+  const handlePress = (element: string) => {
+    console.log(`${element} pressed.`);
   };
 
   return (
     <SafeAreaView style={styles.container}>
       <Text>Hello, React Native!</Text>
-      <Text numberOfLines={3} onPress={handlePress}>
+      <Text numberOfLines={3} onPress={() => handlePress("Text")}>
         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit ullam,
         ab voluptatum distinctio reprehenderit aut cum nihil accusamus, ratione
         quo, molestias provident officiis nulla sunt exercitationem repellat
         quam dolor reiciendis.
       </Text>
-      <Image
-        source={{
-          width: 200,
-          height: 300,
-          uri: "https://picsum.photos/200/300",
-        }}
-      />
+
+      <TouchableHighlight onPress={() => handlePress("Image")}>
+        <Image
+          source={{
+            width: 200,
+            height: 300,
+            uri: "https://picsum.photos/200/300",
+          }}
+        />
+      </TouchableHighlight>
       <StatusBar style="auto" />
     </SafeAreaView>
   );
