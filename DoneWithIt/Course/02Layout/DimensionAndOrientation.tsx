58616e2730cb1f30e2ed4f7905706c8ea6e24a39
Ankur Atri <ankuratri.in@gmail.com>
2023-07-23 22:21:08 +0530

    DimensionAndOrientation example added

---
+++ DoneWithIt/Course/02Layout/DimensionAndOrientation.tsx
@@ -0,0 +1,43 @@
+import { useDeviceOrientation } from "@react-native-community/hooks";
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import {
+  Dimensions,
+  Platform,
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  View,
+  useWindowDimensions,
+} from "react-native";
+
+export default function DimensionAndOrientation() {
+  console.log(Dimensions.get("screen"));
+
+  const orientation = useDeviceOrientation();
+  console.log("orientation:", orientation);
+
+  const dimensions = useWindowDimensions();
+  console.log("dimensions:", dimensions);
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <View
+        style={{
+          backgroundColor: "dodgerblue",
+          width: "100%",
+          height: orientation === "portrait" ? "30%" : "100%",
+        }}
+      ></View>
+      <ExpoStatusBar style="auto" />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+    paddingHorizontal: 20,
+  },
+});
