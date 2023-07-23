38e3175f0aa2c462b7b0896fb570486522454023
Ankur Atri <ankuratri.in@gmail.com>
2023-07-23 22:33:02 +0530

    flex, flexDirection, justifyContent, alignItems, alignSelf example added

--- DoneWithIt/Course/02Layout/Flexbox.tsx
+++ DoneWithIt/Course/02Layout/Flexbox.tsx
@@ -3,14 +3,29 @@ import { View, Text } from "react-native";
 
 const Flexbox = () => {
   return (
-    <View style={{ backgroundColor: "#fff", flex: 1 }}>
-      <View style={{ backgroundColor: "dodgerblue", flex: 2 }}>
+    <View
+      style={{
+        backgroundColor: "#fff",
+        flex: 1,
+        flexDirection: "row",
+        justifyContent: "space-between", // Primary axis, Main, Horizontal, X
+        alignItems: "center", // Cross axis, Secondary, Vertical, Y
+      }}
+    >
+      <View style={{ backgroundColor: "dodgerblue", width: 100, height: 100 }}>
         <Text>1</Text>
       </View>
-      <View style={{ backgroundColor: "gold", flex: 1 }}>
+      <View
+        style={{
+          backgroundColor: "gold",
+          width: 100,
+          height: 100,
+          alignSelf: "flex-end",
+        }}
+      >
         <Text>2</Text>
       </View>
-      <View style={{ backgroundColor: "tomato", flex: 2 }}>
+      <View style={{ backgroundColor: "tomato", width: 100, height: 100 }}>
         <Text>3</Text>
       </View>
     </View>
