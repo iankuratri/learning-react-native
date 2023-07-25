e23f73efdd70abe71cbc037b59a7f8e81c81ba1f
Ankur Atri <ankuratri.in@gmail.com>
2023-07-25 09:41:36 +0530

    Welcome screen added

--- DoneWithIt/App.tsx
+++ DoneWithIt/App.tsx
@@ -1,8 +1,9 @@
 // import ComponentAndStylesheet from "./Course/01FundamentalConcepts/ComponentAndStylesheet";
 // import DimensionAndOrientation from "./Course/02Layout/DimensionAndOrientation";
+// import Flexbox from "./Course/02Layout/Flexbox";
 
-import Flexbox from "./Course/02Layout/Flexbox";
+import WelcomeScreen from "./app/screens/WelcomeScreen";
 
 export default function App() {
-  return <Flexbox />;
+  return <WelcomeScreen />;
 }
