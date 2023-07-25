c5a280820bc53900cb2d00bd80571ec5b691d22a
Ankur Atri <ankuratri.in@gmail.com>
2023-07-25 09:57:44 +0530

    View Image Screen added

--- DoneWithIt/App.tsx
+++ DoneWithIt/App.tsx
@@ -1,9 +1,15 @@
+// Fundamental Concepts
 // import ComponentAndStylesheet from "./Course/01FundamentalConcepts/ComponentAndStylesheet";
+
+// Dimension And Orientation
 // import DimensionAndOrientation from "./Course/02Layout/DimensionAndOrientation";
 // import Flexbox from "./Course/02Layout/Flexbox";
 
-import WelcomeScreen from "./app/screens/WelcomeScreen";
+// Exercise
+// import WelcomeScreen from "./app/screens/WelcomeScreen";
+
+import ViewImageScreen from "./app/screens/ViewImageScreen";
 
 export default function App() {
-  return <WelcomeScreen />;
+  return <ViewImageScreen />;
 }
