b99d4bf90aff2bd4c243ac1b15dd44374e2a307f
Ankur Atri <ankuratri.in@gmail.com>
2023-07-26 16:49:39 +0530

    Listing Details Screen added

--- DoneWithIt/App.tsx
+++ DoneWithIt/App.tsx
@@ -17,24 +17,8 @@
 // import Icons from "./course/03Styling/Icons";
 
 // Imports
-import { View } from "react-native";
-import AppCard from "./app/components/AppCard";
+import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
 
 export default function App() {
-  return (
-    <View
-      style={{
-        backgroundColor: "#f8f4f4",
-        flex: 1,
-        padding: 20,
-        paddingTop: 100,
-      }}
-    >
-      <AppCard
-        title="Red jacket for sale!"
-        subTitle="$100"
-        image={require("./app/assets/jacket.jpg")}
-      />
-    </View>
-  );
+  return <ListingDetailsScreen />;
 }
