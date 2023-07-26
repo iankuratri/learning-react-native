5ba79dc83b2580f04de4552ec03de7ec31223d49
Ankur Atri <ankuratri.in@gmail.com>
2023-07-26 13:50:20 +0530

    App Button component added

--- DoneWithIt/App.tsx
+++ DoneWithIt/App.tsx
@@ -13,9 +13,23 @@
 // import Borders from "./course/03Styling/Borders";
 // import Shadows from "./course/03Styling/Shadows";
 // import PaddingAndMargin from "./course/03Styling/PaddingAndMargin";
-import StylingText from "./course/03Styling/StylingText";
+// import StylingText from "./course/03Styling/StylingText";
 // import Icons from "./course/03Styling/Icons";
 
+// Imports
+import { View } from "react-native";
+import AppButton from "./app/components/AppButton";
+
 export default function App() {
-  return <StylingText />;
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <AppButton title="Login" onPress={() => console.log("Tapped")} />
+    </View>
+  );
 }
