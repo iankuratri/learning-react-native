8cecb44329f0f831b5022113c3944d6d457db13d
Ankur Atri <ankuratri.in@gmail.com>
2023-08-08 09:30:28 +0530

    trying to access camera permission

--- DoneWithIt/App.tsx
+++ DoneWithIt/App.tsx
@@ -1,7 +1,26 @@
-import MessagesScreen from "./app/screens/MessagesScreen";
+import * as ImagePicker from "expo-image-picker";
+import { useEffect } from "react";
+import AppText from "./app/components/AppText";
+import Screen from "./app/screens/Screen";
 
 function App() {
-  return <MessagesScreen />;
+  const requestPermission = async () => {
+    const { granted } = await ImagePicker.requestCameraPermissionsAsync();
+
+    if (granted) {
+      alert("You need to enable permission to access the library.");
+    }
+  };
+
+  useEffect(() => {
+    requestPermission();
+  }, []);
+
+  return (
+    <Screen>
+      <AppText>Image Picker example</AppText>
+    </Screen>
+  );
 }
 
 export default App;
