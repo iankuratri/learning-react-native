18dd8805a6cfcace39fd74cee53f364af1059c04
Ankur Atri <ankuratri.in@gmail.com>
2023-08-12 14:34:23 +0530

    AppFormImagePicker field added on listing edit screen

--- DoneWithIt/App.tsx
+++ DoneWithIt/App.tsx
@@ -1,28 +1,7 @@
-import { useState } from "react";
-import ImageInputList from "./app/components/ImageInputList";
-import Screen from "./app/screens/Screen";
+import ListingEditScreen from "./app/screens/ListingEditScreen";
 
 function App() {
-  const [imageUris, setImageUris] = useState<string[]>([]);
-
-  const handleAdd = (uri: any) => {
-    setImageUris((prevState) => [...prevState, uri]);
-  };
-
-  const handleRemove = (uri: any) => {
-    const newUris = imageUris.filter((imageUri) => imageUri !== uri);
-    setImageUris(newUris);
-  };
-
-  return (
-    <Screen>
-      <ImageInputList
-        imageUris={imageUris}
-        onRemoveImage={handleRemove}
-        onAddImage={handleAdd}
-      />
-    </Screen>
-  );
+  return <ListingEditScreen />;
 }
 
 export default App;
--- DoneWithIt/app/screens/ListingEditScreen.tsx
+++ DoneWithIt/app/screens/ListingEditScreen.tsx
@@ -2,20 +2,22 @@ import React from "react";
 import { StyleSheet } from "react-native";
 import * as Yup from "yup";
 
+import CategoryPickerItem from "../components/CategoryPickerItem";
 import {
   AppForm,
   AppFormField,
   AppFormPicker,
   AppSubmitButton,
 } from "../components/forms";
+import AppFormImagePicker from "../components/forms/AppFormImagePicker";
 import Screen from "../screens/Screen";
-import CategoryPickerItem from "../components/CategoryPickerItem";
 
 const validationSchema = Yup.object().shape({
   title: Yup.string().required().min(1).label("Title"),
   price: Yup.number().required().min(1).max(10000).label("Price"),
   description: Yup.string().label("Description"),
   category: Yup.object().required().nullable().label("Category"),
+  images: Yup.array().min(1, "Please select at least one image."),
 });
 
 const categories = [
@@ -84,10 +86,12 @@ function ListingEditScreen() {
           price: "",
           description: "",
           category: null,
+          images: [],
         }}
         onSubmit={(values) => console.log(values)}
         validationSchema={validationSchema}
       >
+        <AppFormImagePicker name="images" />
         <AppFormField maxLength={255} name="title" placeholder="Title" />
         <AppFormField
           keyboardType="numeric"
