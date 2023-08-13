3d478f5bc427ba04920ed0567c5e96dfd0e65356
Ankur Atri <ankuratri.in@gmail.com>
2023-08-13 10:29:06 +0530

    navigation between screens

--- DoneWithIt/course/06Navigation/StackNavigator.tsx
+++ DoneWithIt/course/06Navigation/StackNavigator.tsx
@@ -1,13 +1,24 @@
-import React from "react";
-import Screen from "./../../app/screens/Screen";
-import AppText from "../../app/components/AppText";
+import { NavigationContainer, useNavigation } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { NavigationContainer } from "@react-navigation/native";
+import React from "react";
+import AppButton from "../../app/components/AppButton";
+import AppText from "../../app/components/AppText";
+import Screen from "./../../app/screens/Screen";
 
-function Tweets() {
+function Goback() {
+  const navigation = useNavigation();
+
+  return <AppButton title="Go back" onPress={() => navigation.goBack()} />;
+}
+
+function Tweets({ navigation }: any) {
   return (
     <Screen>
       <AppText>Tweets</AppText>
+      <AppButton
+        title="View Tweet"
+        onPress={() => navigation.navigate("TweetDetails")}
+      />
     </Screen>
   );
 }
@@ -16,6 +27,7 @@ function TweetDetails() {
   return (
     <Screen>
       <AppText>Tweet Details</AppText>
+      <Goback />
     </Screen>
   );
 }
