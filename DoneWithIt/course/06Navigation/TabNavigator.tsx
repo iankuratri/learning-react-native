0540ed7f94c2b601f55361db39aecf6de669df54
Ankur Atri <ankuratri.in@gmail.com>
2023-08-13 11:09:14 +0530

    Customizing tabs

--- DoneWithIt/course/06Navigation/TabNavigator.tsx
+++ DoneWithIt/course/06Navigation/TabNavigator.tsx
@@ -3,11 +3,12 @@ import { NavigationContainer } from "@react-navigation/native";
 import React from "react";
 import AppText from "../../app/components/AppText";
 import Screen from "./../../app/screens/Screen";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 function Feed() {
   return (
     <Screen>
-      <AppText>Feed</AppText>
+      <AppText>Feed Page</AppText>
     </Screen>
   );
 }
@@ -15,7 +16,7 @@ function Feed() {
 function Account() {
   return (
     <Screen>
-      <AppText>Account Details</AppText>
+      <AppText>Account Details Page</AppText>
     </Screen>
   );
 }
@@ -25,9 +26,37 @@ const Tab = createBottomTabNavigator();
 function TabNavigator() {
   return (
     <NavigationContainer>
-      <Tab.Navigator initialRouteName="Feed">
-        <Tab.Screen name="Feed" component={Feed} />
-        <Tab.Screen name="Account" component={Account} />
+      <Tab.Navigator
+        initialRouteName="Feed"
+        screenOptions={{
+          tabBarActiveBackgroundColor: "tomato",
+          tabBarActiveTintColor: "white",
+          tabBarInactiveBackgroundColor: "#eee",
+          tabBarInactiveTintColor: "black",
+        }}
+      >
+        <Tab.Screen
+          name="Feed"
+          component={Feed}
+          options={{
+            tabBarIcon: ({ size, color }) => (
+              <MaterialCommunityIcons name="home" size={size} color={color} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Account"
+          component={Account}
+          options={{
+            tabBarIcon: ({ size, color }) => (
+              <MaterialCommunityIcons
+                name="account"
+                size={size}
+                color={color}
+              />
+            ),
+          }}
+        />
       </Tab.Navigator>
     </NavigationContainer>
   );
